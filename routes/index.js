8639b94fe25ab3e0b8312df1823b732ab7c49ed5
Petrus Hoc <hoc081098@gmail.com>
2019-02-15 22:57:55 +0700

    Add get /topthang and view count

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const debug = require('debug')('comic-app-server');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -18,6 +19,7 @@ router.get('/truyen_de_cu', async (req, res) => {
     let comics = await truyenDeCu();
     res.status(200).json(comics);
   } catch (e) {
+    debug(e);
     res.status(500).json({
       message: 'Internal error',
       statusCode: 500
@@ -31,6 +33,20 @@ router.get('/truyen_moi_cap_nhat', async (req, res) => {
     let comics = await truyenMoiCapNhat(page);
     res.status(200).json(comics);
   } catch (e) {
+    debug(e);
+    res.status(500).json({
+      message: 'Internal error',
+      statusCode: 500
+    });
+  }
+});
+
+router.get('/top_thang', async (req, res) => {
+  try {
+    let comics = await topThang();
+    res.status(200).json(comics);
+  } catch (e) {
+    debug(e);
     res.status(500).json({
       message: 'Internal error',
       statusCode: 500
@@ -97,14 +113,56 @@ function truyenMoiCapNhat(page) {
           }
         });
 
+        let view = (() => {
+          let html = figure.find('div > div.view > span').html();
+          html = html.replace(/\s{2,}/g, ' ').trim();
+          return /((\d{1,3})(\.)?)+/g.exec(html)[0]
+        })();
+
         comics.push({
           thumbnails: figure.find('div > a > img').attr('data-original'),
           title: $e.find('figcaption > h3 > a').text(),
           chapters: chapters,
           link: figure.find('div > a').attr('href'),
+          view: view,
         });
       });
 
+      resolve(comics);
+    });
+  });
+}
+
+function topThang(page) {
+  return new Promise((resolve, reject) => {
+    let comics = [];
+
+    request.get(`http://www.nettruyen.com/?page=${page}`, (error, response, body) => {
+      if (error) return reject(error);
+
+      let $ = cheerio.load(body);
+      $('div#topMonth').find('li.clearfix').each((i, e) => {
+        let $e = $(e);
+
+        let view = (() => {
+          let html = $e.find('div.t-item > p.chapter > span').html();
+          html = html.replace(/\s{2,}/g, ' ').trim();
+          return /((\d{1,3})(\.)?)+/g.exec(html)[0]
+        })();
+
+        comics.push({
+          thumbnails: $e.find('div.t-item > a > img').attr('data-original'),
+          title: $e.find('div.t-item > h3 > a').text(),
+          chapters: [
+            {
+              chapter_link: $e.find('div.t-item > p.chapter > a').attr('title'),
+              chapter_name: $e.find('div.t-item > p.chapter > a').attr('href'),
+            }
+          ],
+          link: $e.find('div.t-item > a').attr('href'),
+          view: view,
+        });
+      });
       resolve(comics);
     });
   });
