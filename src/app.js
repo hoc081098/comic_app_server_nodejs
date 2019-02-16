8af43b4d2e659c987e34e2e902bedd47699eacac
Petrus Hoc <hoc081098@gmail.com>
2019-02-16 18:51:58 +0700

    some updates

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+import indexRouter from './routes/index';
 
 var app = express();
 
@@ -17,19 +17,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // send the error response
+  let statusCode = err.status || 500;
+  res.status(statusCode)
+    .json({
+      message: `An error occurred: ${err}`,
+      statusCode: statusCode
+    });
 });
 
 module.exports = app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,9 @@
 import { Controller } from '../controllers/index.controller';
 
-const express = require('express');
-const router = express.Router();
+import express, { Router } from 'express';
+const router: Router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.status(200).send('<h3>Hello</h3>')
-});
+router.get('/', (req, res, next) => res.status(200).send('<h3>Hello</h3>'));
 
 router.get('/truyen_de_cu', Controller.truyenDeCu);
 
@@ -14,4 +11,4 @@ router.get('/truyen_moi_cap_nhat', Controller.truyenMoiCapNhat);
 
 router.get('/top_thang', Controller.topThang);
 
-module.exports = router;
+export default router;
