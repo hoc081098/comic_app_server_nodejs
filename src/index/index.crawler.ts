e50156dc5856c32ae7134b312c1fcd7c8af2a104
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-05-01 22:04:34 +0700

    manganelo

--- src/index/index.crawler.ts
+++ src/index/index.crawler.ts
@@ -1,19 +1,19 @@
-import { bodyToComicList, GET } from "../util";
+import { bodyToComicList, bodyToComicListNew, GET } from "../util";
 import { Comic } from "../models/comic.interface";
 
 export class Crawler {
   static async newestComics(page: number): Promise<Comic[]> {
-    const body = await GET(`https://ww2.mangafox.online/newmanga/page/${page}`);
-    return bodyToComicList(body);
+    const body = await GET(`https://manganelo.com/genre-all/${page}?type=newest`);
+    return bodyToComicListNew(body);
   }
 
   static async updatedComics(page: number): Promise<Comic[]> {
-    const body = await GET(`https://ww2.mangafox.online/page/${page}`);
-    return bodyToComicList(body);
+    const body = await GET(`https://manganelo.com/genre-all/${page}`);
+    return bodyToComicListNew(body);
   }
 
   static async mostViewedComics(page: number): Promise<Comic[]> {
-    const body = await GET(`https://ww2.mangafox.online/topmanga/page/${page}`);
-    return bodyToComicList(body);
+    const body = await GET(`https://manganelo.com/genre-all/${page}?type=topview`);
+    return bodyToComicListNew(body);
   }
 }
