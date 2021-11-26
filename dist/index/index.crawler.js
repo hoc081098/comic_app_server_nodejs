c810f7512b945b58b2595c891d7818982056d49b
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-26 18:49:49 +0700

    wip

--- dist/index/index.crawler.js
+++ dist/index/index.crawler.js
@@ -3,16 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Crawler = void 0;
 const util_1 = require("../util");
 class Crawler {
+    static buildUrl(type, page) {
+        const url = `${util_1.BASE_URL}/manga_list/?type=${type}&category=all&state=all&page=${page}`;
+        util_1.log(`GET ${url}`);
+        return url;
+    }
     static async newestComics(page) {
-        const body = await util_1.GET(`https://ww.mangakakalot.tv/manga_list/?type=newest&category=all&state=all&page=${page}`);
+        const body = await util_1.GET(Crawler.buildUrl('newest', page));
         return util_1.bodyToComicListNew(body);
     }
     static async updatedComics(page) {
-        const body = await util_1.GET(`https://manganato.com/genre-all/${page}`);
+        const body = await util_1.GET(Crawler.buildUrl('latest', page));
         return util_1.bodyToComicListNew(body);
     }
     static async mostViewedComics(page) {
-        const body = await util_1.GET(`https://manganato.com/genre-all/${page}?type=topview`);
+        const body = await util_1.GET(Crawler.buildUrl('topview', page));
         return util_1.bodyToComicListNew(body);
     }
 }
--- src/index/index.crawler.ts
+++ src/index/index.crawler.ts
@@ -1,19 +1,25 @@
-import { bodyToComicListNew, GET } from "../util";
+import { BASE_URL, bodyToComicListNew, GET, log } from "../util";
 import { Comic } from "../models/comic.interface";
 
 export class Crawler {
+  static buildUrl(type: string, page: number): string {
+    const url = `${BASE_URL}/manga_list/?type=${type}&category=all&state=all&page=${page}`;
+    log(`GET ${url}`);
+    return url;
+  }
+
   static async newestComics(page: number): Promise<Comic[]> {
-    const body = await GET(`https://ww.mangakakalot.tv/manga_list/?type=newest&category=all&state=all&page=${page}`);
+    const body = await GET(Crawler.buildUrl('newest', page));
     return bodyToComicListNew(body);
   }
 
   static async updatedComics(page: number): Promise<Comic[]> {
-    const body = await GET(`https://manganato.com/genre-all/${page}`);
+    const body = await GET(Crawler.buildUrl('latest', page));
     return bodyToComicListNew(body);
   }
 
   static async mostViewedComics(page: number): Promise<Comic[]> {
-    const body = await GET(`https://manganato.com/genre-all/${page}?type=topview`);
+    const body = await GET(Crawler.buildUrl('topview', page));
     return bodyToComicListNew(body);
   }
 }
